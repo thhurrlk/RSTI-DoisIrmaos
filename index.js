4e56bbefdf3abda6e7d1c9413b79c3875639a15d
thhurrlk <robot24master@gmail.com>
2024-02-23 16:50:14 -0300

    atividades do dia 23

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ pessoa("Lucas")
 pessoa("Pedro")
 pessoa("Ramon")
 
-*/
+*
 
     let pessoas = function(nomedapessoa){
         console.log(nomedapessoa);  
@@ -132,11 +132,167 @@ pessoa("Ramon")
 
 pessoas(prompt("Qual o seu nome?"));
 
+OBJETOS - AULA 7 -----------------------------------------------------------------------------
+
+1-->
+
+const professor = {
+    nome: 'Vitor',
+    idade: 24,
+    email: 'profvitor@gmail.com',
+    tarefa:['Dar aula', 'Responder dúvidas'],
+    contarPiada: function() {
+        console.log('É pa vê ou pa comê?');
+    
+    }
+}
+    console.log(professor.nome);
+    console.log(professor.idade);
+
+    professor.nome = 'Arthur';
+    professor['email'] = 'profarthur@gmail.com';
+    professor.idade = '17';
+
+    console.log(professor.nome);
+    console.log(professor.idade);
+
+    ----------------------------------------------------------------------------------------------------------
+
+    const filme = {
+        Filme: 'Need for Speed - O filme',
+        Lançamento: '13 de Março de 2014',
+        Diretor: 'Scott Waugh',
+        Elenco: ['Aaron Paul', 'Imogen Poots', 'Dominic Cooper', 'Rami Malek', 'Kid Cudi', 'Dakota Johnson', 'Michael Keaton'],
+        Assistido: true
+        
+
+    }
+
+    console.log(filme.Filme);
+    console.log(filme.Lançamento);
+    console.log(filme.Diretor);
+    console.log(filme.Elenco);
+    console.log(filme.Assistido);
+
+    -------------------------------------------------------------------------------------------
+
+
+    const pessoa = {
+
+        nome: 'Maria',
+        idade: '15',
+        genero: 'mulher',
+        musica: 'funk'
+    }
+        console.log("O nome da pessoa é ", pessoa.nome,", ela tem ", pessoa.idade," anos e gosta muito de",pessoa.musica);
+
+*/
+
+/* const donoDoPet = {
+    nome: "Vitor Hugo",
+    pet: {
+    nomeDoPet: "Wanda",
+    raca: "Vira-lata",  
+    idade: 1
+
+    }
+}
+
+    console.log(donoDoPet.pet.nomeDoPet) //Wanda 
+
+    ------------------------------------------------------------------
+    
+   /* const curso = {
+    nome: "Noturno Frontend",
+    linguagens: ["JS", "CSS", "HTML"]
+   }
+
+   console.log(curso.linguagens[0]) 
+   -----------------------------------------------------------------
+/* 
+   const professores = [
+    {nome: "Andrei", modulo: 1},
+    {nome: "Vitor", modulo: 2},
+    {nome: "Mina", modulo: 3}
+   ]
+
+
+   console.log(professores[0])
+ -----------------------------------------------------------------
+
+   /* const curso = {
+    nome: "Frontend",
+    linguagens: ["JS", "CSS", "HTML"]
+
+   }
+
+   curso.numeroEstudantes = 50
+
+   console.log(curso.numeroEstudantes) 
+
+   -----------------------------------------------------------------
+  
+  /* 
+    const filmeelenco ={
+        pessoas: ['Aaron Paul', 'Imogen Poots', 'Dominic Cooper', 'Rami Malek'],
+        nome: ['Tobey Marshall', 'Julia Maddon', 'Dino Brewster', 'Finn'],
+
+    }
+
+
+    console.log(filmeelenco.pessoas[0] + " - " +filmeelenco.nome[0]);
+    console.log(filmeelenco.pessoas[1] + " - " + filmeelenco.nome[1]);
+    console.log(filmeelenco.pessoas[2] + " - " + filmeelenco.nome[2]);
+    console.log(filmeelenco.pessoas[3] + " - " + filmeelenco.nome[3]);
+
+    filmeelenco.pessoas[0] = 'xuxa';
+
+    console.log(filmeelenco)
+
+    console.log(`%cSEXO`, `color: blue;font-size: 35px` )  
+
+    -----------------------------------------------------------------------------*/
+
+    /* const usuario = {
+        nome: 'Prof',
+        idade: 25,
+        email: 'prof@senacrs.gov.br',
+        cidade: 'São Paula'
+    }
+
+    const novoUsuario = {
+        ...usuario,
+        nome: 'João',
+        idade: 28
+    }
+
+    const listaDeNomes = ["Mika", "Paula", "Vitor"];
+    const copiaListaDeNomes = [...listaDeNomes]
+
+    console.log(copiaListaDeNomes) 
+
+    --------------------------------------------------------------------------------*/
+        //EXERCICIO
+
+        const pessoa = {
+
+            nome: 'Maria',
+            idade: '15',
+            genero: 'mulher',
+            musica: 'funk',
+            comidas: ['Churrasco', 'Strogonoff', 'Açaí', 'Frango Frito'],
+            melhorAmiga: {
+                nomeDaAmiga: "Arthur",               
+                idade: 17,
+        
+        }
+
+        console.log(`O nome da pessoa é ${pessoa.nome}`)
 
 
 
 
-
+    
 
 
 
